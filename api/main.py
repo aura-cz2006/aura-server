9866a8e28f198f53418cf5eff5f98bc3380a3e9e
Dyllon <dyllon.g@outlook.com>
2022-03-27 02:44:54 +0800

    Added fastapi

---
+++ api/main.py
@@ -0,0 +1,20 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"isHealthy": True, "caresAboutYou": "❤️", }
+
+
+@app.get("/buses")
+def read_buses():
+    return {"routes": []}
+
+
+# debug
+def start_dev():
+    """Launched with `poetry run start_dev` at root level"""
+    uvicorn.run("api.main:app", host="0.0.0.0", port=80, reload=True)
