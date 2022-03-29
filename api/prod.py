a73f2fa0c8887ce7b1bc37c4c44f4f56f0df2f1a
Dyllon <dyllon.g@outlook.com>
2022-03-30 01:25:20 +0800

    Add cors wildcard

---
+++ api/prod.py
@@ -0,0 +1,21 @@
+from api import main
+
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
+from fastapi.middleware.cors import CORSMiddleware
+
+app = main.app
+
+print("adding production middlware")
+app.add_middleware(
+    TrustedHostMiddleware, allowed_hosts=["aura-app.xyz", "*.aura-app.xyz"]
+)
+app.add_middleware(GZipMiddleware, minimum_size=1000)
+app.add_middleware(
+    CORSMiddleware,
+    # allow_origins=origins,
+    allow_origin_regex='https?://.*',
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
